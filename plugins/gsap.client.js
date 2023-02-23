a8f42e031298b332e1f7cc4a9ee20bf313618534
Vasyl <up.vasyl@gmail.com>
2023-02-23 17:01:09 +0200

    t

--- plugins/gsap.client.js
+++ plugins/gsap.client.js
@@ -14,10 +14,10 @@ export default defineNuxtPlugin(async (nuxtApp) => {
 		let vh
 		vh = window.innerHeight * 0.01
 		document.documentElement.style.setProperty("--vh", `${vh}px`)
-		window.addEventListener('resize', () => {
-			vh = window.innerHeight * 0.01
-			document.documentElement.style.setProperty("--vh", `${vh}px`)
-		})
+		// window.addEventListener('resize', () => {
+		// 	vh = window.innerHeight * 0.01
+		// 	document.documentElement.style.setProperty("--vh", `${vh}px`)
+		// })
 	}
 
 	if (ScrollTrigger.isTouch !== 1) {
