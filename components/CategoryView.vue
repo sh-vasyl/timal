9f8aab4680b604411a09e5c0697547e1a33e74cc
Vasyl <up.vasyl@gmail.com>
2023-02-23 16:36:46 +0200

    test

--- components/CategoryView.vue
+++ components/CategoryView.vue
@@ -4,6 +4,10 @@
 		let vh
 		vh = window.innerHeight * 0.01
 		document.documentElement.style.setProperty("--vh", `${vh}px`)
+		window.addEventListener('resize', () => {
+			vh = window.innerHeight * 0.01
+			document.documentElement.style.setProperty("--vh", `${vh}px`)
+		})
 	})
 </script>
 <template>
