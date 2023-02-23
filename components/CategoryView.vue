f8dfb580f87f295aabfc151eef9b6ee5eeae813e
Vasyl <up.vasyl@gmail.com>
2023-02-23 11:02:08 +0200

    some fixes

--- components/CategoryView.vue
+++ components/CategoryView.vue
@@ -1,3 +1,11 @@
+<script setup>
+
+	tryOnMounted(() => {
+		let vh
+		vh = window.innerHeight * 0.01
+		document.documentElement.style.setProperty("--vh", `${vh}px`)
+	})
+</script>
 <template>
 
 	<div class="category-view">
@@ -9,7 +17,7 @@
 <style lang="scss" scoped>
 .category-view {
 	position: relative;
-	height: 100vh;
+	height: calc(var(--vh, 1vh) * 100)
 }
 
 </style>
