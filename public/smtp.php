63a7010c4707c6903636e83995563f2dcf023a88
Vasyl <up.vasyl@gmail.com>
2023-03-02 16:28:20 +0200

    test

--- public/smtp.php
+++ public/smtp.php
@@ -3,9 +3,9 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
+require '../phpmailer/src/Exception.php';
+require '../phpmailer/src/PHPMailer.php';
+require '../phpmailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
